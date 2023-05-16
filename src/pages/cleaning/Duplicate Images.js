6d63c135d7dca1b101a453d88c2e612eef2d500f
Your Name <dunghuynh110496@gmail.com>
2023-05-16 16:39:33 +1000

    add user id

--- src/pages/cleaning/Duplicate Images.js
+++ src/pages/cleaning/Duplicate Images.js
@@ -5,18 +5,39 @@ import 'katex/dist/katex.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 
-function ImageGrid() {
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [showParagraphs, setShowParagraphs] = useState(false);
+  const [Warning, setWarning] = useState(false);
   const [Subimages, setSubImages] = useState([]);
   const [CenterImage, setCenterImage] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const number = parseInt(inputValue, 10);
+    console.log(number)
+    if (number >= 1 && number <= 40) {
+      
+      axios.get('/duplicate_images/' + number).then(res => {
+        console.log(res.data)
+        if ( res.data != "None" ) {
+          setSubImages(res.data.sub_imgs);
+          setCenterImage(res.data.center_image);
+          setShowParagraphs(true)
+          setWarning(true)
+        } else {
+          setSubImages([]);
+          setCenterImage([]);
+          setShowParagraphs(false)
+          setWarning(true)
+        }
+        
+      })
 
-  useEffect(() => {
-    axios.get('/duplicate_images').then(res => {
-      console.log(res.data)
-      setSubImages(res.data.sub_imgs);
-      setCenterImage(res.data.center_image);
-    });
-  }, []);
+    } else {
+      setShowParagraphs(false);
+    }
+  };
 
   const handleCenterInvalidDelete = () => {
     const updatedImage = { ...CenterImage, valid: 0 };
@@ -58,24 +79,36 @@ function ImageGrid() {
   
 
   const handleSubImageDelete = (center_id, sub_id) => {
-    
     setSubImages(Subimages.filter(sub_image => sub_image.id !== sub_id))
-    console.log(sub_id)
     axios.post('/deleteSubDuplicateImages/' +center_id + "/"+ sub_id)
     };
   
   const handleDone = (center_id) => {
     axios.post('/doneduplicate/' + center_id)
-    window.location.reload();
+    axios.get('/duplicate_images/' + inputValue).then(res => {
+      setSubImages(res.data.sub_imgs);
+      setCenterImage(res.data.center_image);
+    })
+    
     };
 
-
-  
   return (
-    <>
+    <div>
       <h1 class="text-center m-4" style={{color: "red"}}>Remove Duplicate Images</h1>
-      <div class="card" style={{width: "600px", height:"550px", margin: "0 auto", float: "none", "margin-bottom": "10px"}}>
-          <img style={{width: "450x", height:"490px"}} key={CenterImage.id} class="m-2" src={CenterImage.img} alt={"sub_image"}/>.
+      <form class="text-center m-4" onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" >Submit</button>
+      </form>
+
+      {showParagraphs ? (
+        <>
+        <div class="card" style={{width: "600px", height:"550px", margin: "0 auto", float: "none", "margin-bottom": "4px"}}>
+          <img style={{width: "450x", height:"490px"}} key={CenterImage.id} src={CenterImage.img} alt={"sub_image"}/>.
+          <p>Image ID: {CenterImage.id}</p>
           <div class="card-body text-center">
           {CenterImage.valid === 2 ? (
             <button class="btn btn-danger btn-lg m-2"
@@ -95,6 +128,7 @@ function ImageGrid() {
               <div class="card" style={{width: "500px", height:"380px"}}>
               <img key={sub_image.id} class="m-2" src={sub_image.img} alt={"sub_image"} style={{width: "450px", height:"360px"}}
                 onDoubleClick={() => handleSubImageDelete(CenterImage.id, sub_image.id)}/>
+              <p>Image ID: {sub_image.id}</p>
               </div>
               <div class="card-body text-center">
               {sub_image.valid === 2 ? (
@@ -113,8 +147,14 @@ function ImageGrid() {
           <button class="btn btn-primary btn-lg m-2" 
             onClick={() => handleDone(CenterImage.id)}> Done! </button>
       </div>
-    </>
+        </>
+      ) : (null)}
+      {showParagraphs ? (null
+      ) :(<p style={{color: "red"}} class="text-center m-4">Invalid value</p>)}
+      {Warning ? (<p style={{color: "red"}} class="text-center m-4">Done!</p>
+      ) :(null)}
+    </div>
   );
 }
 
-export default ImageGrid;
+export default App;

d39aa78cceba05ac9fe1742c8fcf7e0f50bbe9e6
Ngoc Dung Huynh <63529910+parkerhuynh@users.noreply.github.com>
2023-05-18 01:48:44 +1000

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,7 @@ import pymysql.cursors
 def connect_to_mysql():
     return pymysql.connect(host='localhost',
                            user='root',
-                           password='root',
+                           password='Ngocdung242!',
                            db='cartoon',
                            charset='utf8mb4',
                            cursorclass=pymysql.cursors.DictCursor)

0e068cc44ef69369893a1704a5c1280ad4b23aa2
Your Name <dunghuynh110496@gmail.com>
2023-05-25 15:01:08 +1000

    add Promt Analysis

---
+++ src/pages/data/Promt.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+const IFrame = ({ url }) => {
+    const [iFrameLoaded, setIFrameLoaded] = React.useState(false);
+    const divBoxRef = React.useRef(null);
+    const resizeIframe = (iframe) => {
+        const { documentElement } = iframe.contentWindow.document;
+        const height = documentElement.scrollHeight;
+        iframe.style.height = '8000px';
+        iframe.style.width = '100%';
+        
+    };
+    return (
+      <div ref={divBoxRef} class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+            {!iFrameLoaded && (
+                <span className="inline-block">loading notebook ...</span>
+                )}
+                <iframe
+                onLoad={(e) => {
+                    setIFrameLoaded(true);
+                    resizeIframe(e.target);
+                }}
+                title="static_html"
+                src={"./Promt.html"}
+                ></iframe>
+            </div>
+            <div class="col-1"></div>
+        </div>
+        
+      </div>
+    );
+  };
+  export default IFrame;
